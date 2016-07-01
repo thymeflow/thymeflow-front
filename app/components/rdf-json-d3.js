b649120a92ea2319cd77975fa5ee5aff5cbdd98c
David Montoya <masda70@gmail.com>
2016-07-01 14:21:15 +0200

    - rdf-json-d3 display images

--- app/components/rdf-json-d3.js
+++ app/components/rdf-json-d3.js
@@ -2,6 +2,7 @@ import Ember from "ember";
 import d3 from "d3";
 
 const typeProperty = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+const imageProperty = "http://schema.org/image";
 
 export default Ember.Component.extend({
   tagName: "div",
@@ -94,15 +95,17 @@ export default Ember.Component.extend({
     }
   },
   onUpdateRdfJson: function () {
+    const viewSize = 4000;
+    const imageSize = 50;
     this.cleanVisualization();
-
+    const removePrefix = this.get('removePrefix');
     const graph = this.rdfToGraph(this.get('rdfJson'));
 
     const tip = d3.tip().attr('class', 'graph-tooltip n').html(function (d) {
       const root = Ember.$("<div/>");
       root.append(Ember.$("<div/>").text(d.value));
       for (var [property, literals] of d.properties.entries()) {
-        root.append(Ember.$("<div/>").text(`${property}: ${literals.map(literal => `"${literal.value}" (${literal.type})`)}`));
+        root.append(Ember.$("<div/>").text(`${removePrefix(property)}: ${literals.map(literal => `"${literal.value}" (${literal.type})`)}`));
       }
       return root.html();
     });
@@ -116,14 +119,18 @@ export default Ember.Component.extend({
 
     const force = d3.layout.force()
       .charge(-200)
-      .linkDistance(100)
-      .size([4000, 4000]);
+      .linkDistance(300)
+      .size([viewSize, viewSize]);
+
+    function onTransform(d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    }
 
     const svgBase = d3.select(this.$().get(0)).append("svg");
 
     const svg = svgBase.attr("class", "svg-content-responsive")
       .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("viewBox", "0 0 4000 4000")
+      .attr("viewBox", `0 0 ${viewSize} ${viewSize}`)
       .call(d3.behavior.zoom().on("zoom", function () {
         svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
       }))
@@ -149,15 +156,38 @@ export default Ember.Component.extend({
 
     const node = svg.selectAll(".node")
       .data(graph.nodes)
-      .enter().append("circle")
+      .enter().append("g")
+      .call(force.drag)
+      .on('click', connectedNodes)
+      .attr("transform", onTransform)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+    node.append("circle")
       .attr("class", "graph-node")
       .attr("r", 6)
       .style("fill", function (d) {
         return nodeTypeColor(d.type);
+      });
+
+    node.filter(function (d) {
+      return d.properties.has(imageProperty);
+    }).append("image")
+      .attr("xlink:href", function (d) {
+        if (d.properties.has(imageProperty)) {
+          return d.properties.get(imageProperty)[0].value;
+        } else {
+          return "#";
+        }
       })
-      .call(force.drag)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
+      .attr("x", function () {
+        return -imageSize / 2;
+      })
+      .attr("y", function () {
+        return -imageSize / 2;
+      })
+      .attr("height", imageSize)
+      .attr("width", imageSize);
 
     force.on("tick", function () {
       link.attr("x1", function (d) {
@@ -173,13 +203,34 @@ export default Ember.Component.extend({
           return d.target.y;
         });
 
-      node.attr("cx", function (d) {
-          return d.x;
-        })
-        .attr("cy", function (d) {
-          return d.y;
-        });
+      node.attr("transform", onTransform);
     });
+
+    //Toggle stores whether the highlighting is on
+    var toggle = 0;
+    var neighbors = new Map();
+    graph.nodes.forEach(node => neighbors.set(node.value, new Set([node.value])));
+    graph.links.forEach(function (d) {
+      neighbors.get(d.source.value).add(d.target.value);
+      neighbors.get(d.target.value).add(d.source.value);
+    });
+    function connectedNodes() {
+      if (toggle === 0) {
+        //Reduce the opacity of all but the neighbouring nodes
+        const d = d3.select(this).node().__data__;
+        node.style("opacity", function (o) {
+          return neighbors.get(d.value).has(o.value) ? 1 : 0.1;
+        });
+        link.style("opacity", function (o) {
+          return d.value === o.source.value || d.value === o.target.value ? 1 : 0.1;
+        });
+        toggle = 1;
+      } else {
+        node.style("opacity", 1);
+        link.style("opacity", 1);
+        toggle = 0;
+      }
+    }
 
     this.set('node', node);
     this.set('link', link);
