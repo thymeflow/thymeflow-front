6d74b8902f2bae34a732d5c83a7b9de9a9e4cd1e
David Montoya <masda70@gmail.com>
2016-08-31 18:44:14 +0200

    fix build: remove unused import.

--- app/components/timeline/timeline-details-item-row.js
+++ app/components/timeline/timeline-details-item-row.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
-import moment from 'moment';
 
 export default Ember.Component.extend({
   tagName: 'tr',
   classNames: ['timeline-row'],
   classNameBindings: ['selected:table-info'],
-  elementInserted: false,
   observedSelected: (function (){
     const selected = this.get('selected');
     if(selected){
