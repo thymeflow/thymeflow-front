370d16911d4d493f58391daf0a377d6e7300e7f2
FrancoisCamus <francois.camus@gmail.com>
2016-09-19 14:19:09 +0200

    Revert "Added system task date formatting"
    This reverts commit 8656cf29848024e0f5669b19710c7588313434c0.

--- app/helpers/system-task-date.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function systemTaskDate([date]) {
-  return moment(date).format('MM/DD/YYYY - hh:mm:ss a'); // By default we use US format
-}
-
-export default Ember.Helper.helper(systemTaskDate);
