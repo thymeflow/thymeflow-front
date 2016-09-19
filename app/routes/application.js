b132aa834960e8292835fe1afe9080de231a6d13
FrancoisCamus <francois.camus@gmail.com>
2016-09-19 16:57:09 +0200

    Implemented system-tasks polling

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
+const pollingInterval = 30000;
+
 export default Ember.Route.extend({
+  afterModel() {
+
+    let self = this;
+
+    Ember.run.later(function() {
+
+      self.refresh();
+    }, pollingInterval);
+  },
   model() {
 
     return this.get('store').findAll('system-task');
