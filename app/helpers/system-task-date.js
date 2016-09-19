31cb170240a09246012903638aa4649a9e07523d
FrancoisCamus <francois.camus@gmail.com>
2016-09-19 15:09:11 +0200

    Localized system task date

--- app/helpers/system-task-date.js
+++ app/helpers/system-task-date.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function systemTaskDate([date]) {
-  return moment(date).format('MM/DD/YYYY - hh:mm:ss a'); // By default we use US format
+  return moment(date).format('L - LTS');
 }
 
 export default Ember.Helper.helper(systemTaskDate);
