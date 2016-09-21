307ccc3828a61c7db31777d9abb8cea4db13a94b
FrancoisCamus <francois.camus@gmail.com>
2016-09-21 16:23:58 +0200

    Switched to generic fix for dashed attr

--- app/serializers/system-task.js
+++ app/serializers/system-task.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  attrs: {
-    startDate: 'startDate' // Temporary fix for https://github.com/thymeflow/thymeflow-back/issues/2
+  keyForAttribute: function(attr) {
+    return attr;
+  },
+  keyForRelationship(key) {
+    return key;
   }
 });
