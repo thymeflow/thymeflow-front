c8d99b777e10a7cca55fba07f7c9c3ec7e6eed46
FrancoisCamus <francois.camus@gmail.com>
2016-09-21 11:39:19 +0200

    Associated activity icon with running tasks

--- app/components/system-status.js
+++ app/components/system-status.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  
+  hasRunningTasks: Ember.computed('tasks.@each.progress', function() {
+
+    let tasks = this.get('tasks');
+
+    if (!tasks || tasks.get('length') === 0) {
+      return false;
+    }
+
+    return tasks.any(function(item) {
+
+      let progress = item.get('progress');
+
+      return !progress || (progress && progress !== 100);
+    });
+  })
 });
