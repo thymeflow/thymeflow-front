62d6e7b2f9ef57cfbc9eab1c73b719e5e486e536
FrancoisCamus <francois.camus@gmail.com>
2016-10-20 14:56:14 +0200

    Fixed system-tasks polling mechanism

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,18 +3,18 @@ import ENV from 'thymeflow-front/config/environment';
 
 export default Ember.Route.extend({
   afterModel() {
-
-    let self = this;
-
-    Ember.run.later(function() {
-
-      self.refresh();
-    }, ENV.APP.systemTasksPollingInterval);
+    this.poll();
   },
   model() {
-
     return this.get('store').findAll('system-task').catch(function(error) {
       console.log(error);
     });
+  },
+  poll() {
+    let self = this;
+    Ember.run.later(function() {
+      self.modelFor(self.routeName).update();
+      self.poll();
+    }, ENV.APP.systemTasksPollingInterval);
   }
 });
