4948f53369aff3a785b5b1771a9dad2cae3bb85c
FrancoisCamus <francois.camus@gmail.com>
2016-10-21 15:16:48 +0200

    Added file-service and email-service screen

--- app/models/data-service.js
+++ app/models/data-service.js
@@ -16,5 +16,25 @@ export default DS.Model.extend({
   accounts: DS.attr(),
   accountsCount: Ember.computed('accounts', function() {
     return this.get('accounts').length;
+  }),
+  isExternalService: Ember.computed('name', function() {
+    switch (`${this.get('name')}`) {
+      case 'Google':
+      case 'Microsoft':
+      case 'Facebook': return true;
+      default: return false;
+    }
+  }),
+  isFileService: Ember.computed('name', function() {
+    switch (`${this.get('name')}`) {
+      case 'File': return true;
+      default: return false;
+    }
+  }),
+  isEmailService: Ember.computed('name', function() {
+    switch (`${this.get('name')}`) {
+      case 'Email': return true;
+      default: return false;
+    }
   })
 });
